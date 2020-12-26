817484f877839f1f69fc6a43d19c0083f143b800
santhu-hammer <ssanthujan@yahoo.com>
2020-12-27 01:50:04 +0530

    button

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,59 @@
-import '';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primaryColor: Colors.amber),
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Calculator'),
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.all(10.0),
+                alignment: Alignment.bottomRight,
+                child: Text(
+                  '0',
+                  style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.w700),
+                ),
+              ),
+            ),
+            Row(
+              children: [
+                OutlineButton(
+                  padding: EdgeInsets.all(25.0),
+                  onPressed: () {},
+                  child: Text(
+                    '9',
+                    style: TextStyle(fontSize: 25.0),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
